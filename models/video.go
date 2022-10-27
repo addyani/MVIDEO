9be0e96bb248feeba70d4397e39adf28f1f596e7
Shidqi Ramadhandy Rizqulloh <48822180+CrimsonSZ-Q@users.noreply.github.com>
2022-10-27 20:22:38 +0700

    Add files via upload

--- models/video.go
+++ models/video.go
@@ -13,3 +13,37 @@ type Video struct {
 	DisableAds  bool   `gorm:"default:false"`
 	UserIdVideo uint   `gorm:"foreignKey:UserIdUserIdVideo"`
 }
+
+func CreateVideo(db *gorm.DB, newVideo *Video, userId uint) (err error) {
+	newVideo.UserIdVideo = userId
+	err = db.Create(newCart).Error
+	if err != nil {
+		return err
+	}
+	return nil
+
+func ViewVideo(db *gorm.DB, video *Video, id int) (err error) {
+	err = db.Where("user_id=?", id).ind(video).Error
+	if err != nil {
+		return err
+	}
+	return nil
+	}
+
+func FindVideoById(db *gorm.DB, video *Video, id int) (err error) {
+	err = db.Where("user_id=?", id).First(video).Error
+	if err != nil {
+		return err
+	}
+	return nil
+	}
+func UpdateVideo(db *gorm.DB, video *Video) (err error) {
+	db.Save(video)
+
+	return nil
+	}
+func DeleteVideo(db *gorm.DB, video *Video, id int) (err error) {
+	db.Where("id=?", id).Delete(video)
+
+	return nil
+	}
