fadec573b6012b551b32ad6c9ceae95063ecfa44
Addyani Amirul Fajar <addyani28082000@gmail.com>
2022-10-28 15:40:47 +0700

    user auth

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module ilmudata/task1
 go 1.19
 
 require (
-	github.com/gofiber/fiber/v2 v2.38.1
+	github.com/gofiber/fiber/v2 v2.39.0
 	github.com/gofiber/template v1.7.1
 	golang.org/x/crypto v0.1.0
 	gorm.io/driver/sqlserver v1.4.1
@@ -13,14 +13,20 @@ require (
 require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/gofiber/jwt/v3 v3.3.3 // indirect
+	github.com/golang-jwt/jwt/v4 v4.4.2 // indirect
 	github.com/golang-sql/civil v0.0.0-20220223132316-b832511892a9 // indirect
 	github.com/golang-sql/sqlexp v0.1.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	github.com/klauspost/compress v1.15.11 // indirect
+	github.com/klauspost/compress v1.15.12 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
 	github.com/microsoft/go-mssqldb v0.17.0 // indirect
+	github.com/rivo/uniseg v0.4.2 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasthttp v1.40.0 // indirect
+	github.com/valyala/fasthttp v1.41.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/sys v0.1.0 // indirect
 	gorm.io/driver/mysql v1.4.3 // indirect
--- models/user.go
+++ models/user.go
@@ -1,48 +1,45 @@
 package models
 
-import (
-	"gorm.io/gorm"
-)
-
-type User struct {
-	gorm.Model
-	Id       int    `form:"id" json:"id" validate:"required"`
-	Name     string `form:"name" json:"name" validate:"required"`
-	Username string `form:"username" json:"username" validate:"required"`
-	Image    string `form:"image" json:"image" validate:"required"`
-	Email    string `form:"email" json:"email" validate:"required"`
-	Role     string `form:"role" json:"role" validate:"required"`
-	Password string `form:"password" json:"password" validate:"required"`
-	Disable  bool
-	Videos   []*Video  `gorm:"foreignKey:UserIdVideo"`
-	Adverts  []*Advert `gorm:"foreignKey:UserIdAdvert"`
-}
+// type User struct {
+// 	gorm.Model
+// 	Id       int    `form:"id" json:"id" validate:"required"`
+// 	Name     string `form:"name" json:"name" validate:"required"`
+// 	Username string `form:"username" json:"username" validate:"required"`
+// 	Image    string `form:"image" json:"image" validate:"required"`
+// 	Email    string `form:"email" json:"email" validate:"required"`
+// 	Role     string `form:"role" json:"role" validate:"required"`
+// 	Password string `form:"password" json:"password" validate:"required"`
+// 	Disable  bool
+// 	Videos   []*Video  `gorm:"foreignKey:UserIdVideo"`
+// 	Adverts  []*Advert `gorm:"foreignKey:UserIdAdvert"`
+// }
 
 type LoginForm struct {
 	Username string `form:"username" json:"username" validate:"required"`
 	Password string `form:"password" json:"password" validate:"required"`
+	Name     string `form:"name" json:"name" validate:"required"`
 }
 
-func CreateUser(db *gorm.DB, newUser *User) (err error) {
-	err = db.Create(newUser).Error
-	if err != nil {
-		return err
-	}
-	return nil
-}
+// func CreateUser(db *gorm.DB, newUser *User) (err error) {
+// 	err = db.Create(newUser).Error
+// 	if err != nil {
+// 		return err
+// 	}
+// 	return nil
+// }
 
-func FindUserByUsername(db *gorm.DB, user *User, username string) (err error) {
-	err = db.Where("username=?", username).First(user).Error
-	if err != nil {
-		return err
-	}
-	return nil
-}
+// func FindUserByUsername(db *gorm.DB, user *User, username string) (err error) {
+// 	err = db.Where("username=?", username).First(user).Error
+// 	if err != nil {
+// 		return err
+// 	}
+// 	return nil
+// }
 
-func FindUserById(db *gorm.DB, user *User, id int) (err error) {
-	err = db.Where("id=?", id).First(user).Error
-	if err != nil {
-		return err
-	}
-	return nil
-}
+// func FindUserById(db *gorm.DB, user *User, id int) (err error) {
+// 	err = db.Where("id=?", id).First(user).Error
+// 	if err != nil {
+// 		return err
+// 	}
+// 	return nil
+// }
